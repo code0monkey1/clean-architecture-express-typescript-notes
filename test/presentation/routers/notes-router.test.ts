537db41712c412952e23632e060b10b7fc4eaced
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-29 10:24:09 +0000

    29 Jun 2023, 15:54

--- test/presentation/routers/notes-router.test.ts
+++ test/presentation/routers/notes-router.test.ts
@@ -1,7 +1,55 @@
+import { NoteRequestModel, NoteResponseModel } from "../../../src/domain/entities";
+import { CreateNoteUseCase } from "../../../src/domain/interfaces/use-cases/create-note-use-case";
+import { GetAllNotesUseCase } from '../../../src/domain/interfaces/use-cases/get-all-notes-use-case';
+import { UpdateNoteUseCase } from '../../../src/domain/interfaces/use-cases/update-note-use-case';
+
 describe('Notes Router', () => {
      
-     describe('GET / note', () => {
+     class MockCreateNoteUseCase implements CreateNoteUseCase{
+       execute(note: NoteRequestModel): Promise<NoteResponseModel> {
+         throw new Error("Method not implemented.");
+       }
+      
+     }
 
+     class MockUpdateNoteUseCase implements UpdateNoteUseCase{
+       execute(id: string, note: Partial<NoteRequestModel>): Promise<NoteResponseModel> {
+         throw new Error("Method not implemented.");
+       }
+      
+     }
+
+     class MockGetAllNotesUseCase implements GetAllNotesUseCase{
+       execute(): Promise<NoteResponseModel[]> {
+         throw new Error("Method not implemented.");
+       }
+
+      
+     }
+
+     beforeEach(()=>{
+
+      jest.clearAllMocks()
+      
+      mockCreateNoteUseCase= new MockCreateNoteUseCase()
+      mockGetAllNotesUseCase= new MockGetAllNotesUseCase()
+      mockUpdateNoteUseCase = new MockUpdateNoteUseCase()
+
+     } )
+    
+   let mockCreateNoteUseCase:CreateNoteUseCase
+   let mockGetAllNotesUseCase:GetAllNotesUseCase
+   let mockUpdateNoteUseCase :UpdateNoteUseCase
+     
+     describe('GET / note', () => {
+      
+        test('should return 200 with data',async()=>{
+           
+              const notes:NoteResponseModel[] = await mockGetAllNotesUseCase.execute()
+
+              
+               
+        })
         
        
      })
